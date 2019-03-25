87222d828ee8181b9b986e1d198fb7b7d23ee2fe
rrios7 <rrios.7@outlook.com>
2019-03-25 14:49:32 -0600

    Se cargan imagenes y precios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  casas = [];
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+    this.http.get('/v1/klfst?&category=1040&region=16&lang=es&lim=15')
+    .subscribe( data => {
+      console.log(JSON.stringify(data));
+      this.casas = data['list_ads'];
+    }, error => {
+      console.log(JSON.stringify(error));
+    });
   }
-
 }
