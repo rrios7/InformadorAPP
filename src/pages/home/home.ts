15bb5de18709628a93c6da696298551e0c46c739
rrios7 <rrios.7@outlook.com>
2019-03-25 21:33:45 -0600

    Correcion de errores, ahora se muestran las casas correctamente

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,13 +8,17 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePage {
   casas = [];
-  constructor(public navCtrl: NavController, public http: HttpClient) {
-    this.http.get('/v1/klfst?&category=1040&region=16&lang=es&lim=15')
-    .subscribe( data => {
-      console.log(JSON.stringify(data));
-      this.casas = data['list_ads'];
-    }, error => {
-      console.log(JSON.stringify(error));
-    });
+
+  constructor(public navCtrl: NavController, 
+    public http: HttpClient) {
+
+      this.http.get('/v1/klfst?&category=1040&region=16&lang=es&lim=15')
+      .subscribe( data => {
+        
+        this.casas = data['list_ads'];
+        console.log(JSON.stringify(this.casas));
+      }, error => {
+        console.log(JSON.stringify(error));
+      });
   }
 }
